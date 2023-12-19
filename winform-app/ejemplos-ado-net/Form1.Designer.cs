ce11cc5f1acd7b4d960e5afbe081befaeee4fef2
David <clcdavi@gmail.com>
2023-12-19 11:19:58 -0300

    app winform-pokemon

---
+++ winform-app/ejemplos-ado-net/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿
+namespace ejemplos_ado_net
+{
+    partial class frmPokemons
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvPokemos = new System.Windows.Forms.DataGridView();
+            this.pBoxPokemon = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPokemos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxPokemon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvPokemos
+            // 
+            this.dgvPokemos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPokemos.Location = new System.Drawing.Point(12, 33);
+            this.dgvPokemos.Name = "dgvPokemos";
+            this.dgvPokemos.Size = new System.Drawing.Size(443, 189);
+            this.dgvPokemos.TabIndex = 0;
+            this.dgvPokemos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvPokemos_CellContentClick);
+            this.dgvPokemos.SelectionChanged += new System.EventHandler(this.dgvPokemos_SelectionChanged);
+            // 
+            // pBoxPokemon
+            // 
+            this.pBoxPokemon.Location = new System.Drawing.Point(462, 33);
+            this.pBoxPokemon.Name = "pBoxPokemon";
+            this.pBoxPokemon.Size = new System.Drawing.Size(229, 189);
+            this.pBoxPokemon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pBoxPokemon.TabIndex = 1;
+            this.pBoxPokemon.TabStop = false;
+            // 
+            // frmPokemons
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pBoxPokemon);
+            this.Controls.Add(this.dgvPokemos);
+            this.Name = "frmPokemons";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPokemos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBoxPokemon)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvPokemos;
+        private System.Windows.Forms.PictureBox pBoxPokemon;
+    }
+}
+
---
+++ winform-app/ejemplos-ado-net/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ejemplos_ado_net
+{
+    public partial class frmPokemons : Form
+    {
+        private List<Pokemon> listaPokemon;
+        public frmPokemons()
+        {
+            InitializeComponent();
+        }
+
+        private void dgvPokemos_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PokemonNegocio negocio = new PokemonNegocio();
+            listaPokemon = negocio.listar();
+            dgvPokemos.DataSource = negocio.listar();
+            cargarImagen(listaPokemon[0].UrlImagen);
+        }
+
+        private void dgvPokemos_SelectionChanged(object sender, EventArgs e)
+        {
+            Pokemon seleccionado = (Pokemon)dgvPokemos.CurrentRow.DataBoundItem;
+            cargarImagen(seleccionado.UrlImagen);
+        }
+
+        private void cargarImagen(string imagen)
+        {
+            try
+            {
+                pBoxPokemon.Load(imagen);
+            }
+            catch (Exception ex)
+            {
+
+                pBoxPokemon.Load("https://www.came-educativa.com.ar/upsoazej/2022/03/placeholder-4.png");
+            }
+        }
+    }
+}
